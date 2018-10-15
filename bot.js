21b97bd2dec3aab3e3fc19c7edc87b420dc55725
shadysss <43673718+shadysss@users.noreply.github.com>
2018-10-15 16:42:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,27 +34,16 @@ message.author.send(`**مدة الرابط : يـوم
 
 
 
-client.on('message' , message => {
-  if(message.content.startsWith('-Voice')){
-         if(!message.member.hasPermission(`MANAGE_GUILD`)) return;
-        message.guild.createChannel('Voice Area', 'category').then(cg => {
-        message.guild.createChannel('Relax', 'voice').then(cha => {
-        message.guild.createChannel('Coffe', 'voice').then(a7aa => {
-        message.guild.createChannel('Cronner', 'voice').then(a7aaa=> {
-        message.guild.createChannel('Music', 'voice').then(a7aaaa => {
-            cha.setParent(cg)
-            a7aa.setParent(cg)
-            a7aaa.setParent(cg)
-            a7aaaa.setParent(cg)
+client.on('message', message => {
+    if(message.content == '!bans'){
+        message.guild.fetchBans().then(bans => {
+            bans.forEach(user => {
+               message.channel.send('\`#\` <@'+ user.id + '>');
+            });
+        });
+    }
+});
 
-                           message.reply('**I Make Voice Rooms **')
-        });})
-           })
-})
-})
-
-}
-          });
 
 
 
