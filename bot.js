0b7884968a55218eef709a6e2de08e06d24d1a4c
shadysss <43673718+shadysss@users.noreply.github.com>
2018-10-15 16:38:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,5 +33,30 @@ message.author.send(`**مدة الرابط : يـوم
 });
 
 
+
+client.on('message' , message => {
+  if(message.content.startsWith('-Voice')){
+         if(!message.member.hasPermission(`MANAGE_GUILD`)) return;
+        message.guild.createChannel('Voice Area', 'category').then(cg => {
+        message.guild.createChannel('Relax', 'voice').then(cha => {
+        message.guild.createChannel('Coffe', 'voice').then(a7aa => {
+        message.guild.createChannel('Cronner', 'voice').then(a7aaa=> {
+        message.guild.createChannel('Music', 'voice').then(a7aaaa => {
+            cha.setParent(cg)
+            a7aa.setParent(cg)
+            a7aaa.setParent(cg)
+            a7aaaa.setParent(cg)
+
+                           message.reply('**I Make Voice Rooms **')
+        });})
+           })
+})
+})
+
+}
+          });
+
+
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
