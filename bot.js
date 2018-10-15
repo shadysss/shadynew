bbc9362d133bcff815f62c58746b3185b68a1344
shadysss <43673718+shadysss@users.noreply.github.com>
2018-10-15 16:29:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,24 @@ client.on('message', message => {
 
 
 
+client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
 
+  message.channel.createInvite({
+        thing: true,
+        maxUses: 100,
+        maxAge: 86400
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+  message.channel.send("**تم ارسال الرابط برسالة خاصة**")
+
+message.author.send(`**مدة الرابط : يـوم
+عدد استخدامات الرابط : 100**`)
+
+
+    }
+});
 
 
 // THIS  MUST  BE  THIS  WAY
